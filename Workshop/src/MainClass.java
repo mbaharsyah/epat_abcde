f83a4db6089ebeeea410b451347e061120b9d557
issuser <issuser@ISS-CR3113.stf.nus.edu.sg>
2018-07-18 14:11:28 +0800

    Test

---
+++ Workshop/src/MainClass.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MainClass {
+	public static void main(String[] args){
+		boolean exit = false;
+		do {
+			showMenu();
+			int input = readInput();
+			if (input==9) exit=true;
+		} while (!exit);
+	}
+	
+	public static void showMenu(){
+		System.out.println("Welcome to To Do List Application");
+		showCurrentList();
+		System.out.println("Menu: ");
+		System.out.println("1. List all to do list");
+		System.out.println("2. Add new item");
+		System.out.println("3. Delete an item");
+		System.out.println("4. Mark an item as complete");
+		System.out.println("9. Quit");
+		System.out.print("Enter your choice: ");
+	}
+	
+	public static int readInput(){
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();	
+		
+		switch(input){
+		case 1:
+			System.out.println("1");
+			break;
+			
+		case 2:
+			System.out.println("2");
+			break;
+			
+		case 3:
+			System.out.println("3");
+			break;
+			
+		case 9:
+
+			break;
+		default:
+			System.out.println("Invalid choice");
+		}
+		
+		
+		return input;
+	}
+	
+	public static void showCurrentList(){
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Fix bug no 1");
+		list.add("Fix bug no 2");
+		list.add("Fix bug no 3");
+		
+		for (String s : list){
+			System.out.println(s);
+		}
+		
+	}
+}
